8fe949b040b0fb7acbd404e28445dc108e05514b
Stewart <44583600+stewheart123@users.noreply.github.com>
2024-02-06 23:09:42 +0000

    package alter

---
+++ words/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.word = void 0;
+var word = exports.word = function word(input) {
+  return "Option ".concat(input, "!");
+};
